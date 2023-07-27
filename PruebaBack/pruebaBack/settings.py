115bb45347a345a6f21129b3547183414a0c7635
ionydeodo <jhony.atehortua@alternova.io>
2023-07-27 13:55:03 -0500

    erorr

--- PruebaBack/pruebaBack/settings.py
+++ PruebaBack/pruebaBack/settings.py
@@ -124,17 +124,6 @@ DATABASES = {
     }
 }
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
