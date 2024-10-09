d1a8c79a0cde583c6ae92bbe85381be23e348ef1
jhony alexis atehortua mejia <73616579+ionydeodo@users.noreply.github.com>
2024-10-09 10:18:48 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -118,7 +118,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pruebaBack',
         'USER': 'postgres',
-        'PASSWORD': 'examine2405',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
